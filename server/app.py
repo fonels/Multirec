1df03de85f5556de1fac678b3e4af9761cb1f1a9
fonel <51994351+fonels@users.noreply.github.com>
2025-01-13 22:47:08 +0500

    Delete Procfile

--- server/app.py
+++ server/app.py
@@ -1,11 +1,22 @@
-# server/app.py
-from flask import Flask
+from flask import Flask, render_template
+from flask_cors import CORS
+from .routes import api
+import os
 
-app = Flask(__name__)
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+TEMPLATE_FOLDER = os.path.join(BASE_DIR, 'client')
+STATIC_FOLDER = os.path.join(BASE_DIR, 'client/static')
 
-@app.route("/")
-def hello():
-    return "Hello from Gunicorn on 8080!"
+app = Flask(__name__, template_folder=TEMPLATE_FOLDER,static_folder=STATIC_FOLDER)
+CORS(app)
 
-if __name__ == "__main__":
-    app.run(port=8080)
+app.register_blueprint(api, url_prefix='/api')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+port = int(os.environ.get("PORT", 5000))
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=port)
+print(os.environ)
