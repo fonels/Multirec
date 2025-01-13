430ccb15e3852ae374a07a6e0305b4b61373dd9f
fonel <51994351+fonels@users.noreply.github.com>
2025-01-13 17:47:07 +0500

    Replace app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+from server.routes import api
+from server.utils.config import Config
+import os
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+TEMPLATE_FOLDER = os.path.join(BASE_DIR, 'client')
+STATIC_FOLDER = os.path.join(BASE_DIR, 'client/static')
+
+app = Flask(__name__, template_folder=TEMPLATE_FOLDER,static_folder=STATIC_FOLDER)
+CORS(app)
+
+app.register_blueprint(api, url_prefix='/api')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=Config.FLASK_DEBUG, host=Config.FLASK_HOST, port=Config.FLASK_PORT)
